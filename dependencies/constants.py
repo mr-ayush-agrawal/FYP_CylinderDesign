5e336f34d2cc7c01c406937f9823e398dadf8901
Ayush <me21b1076@iiitdm.ac.in>
2025-05-07 18:08:41 +0530

    Limit update

--- dependencies/constants.py
+++ dependencies/constants.py
@@ -1,10 +1,10 @@
 # Defining some constants for ranges 
 
 HELICAL_ANGLE_LB = 10
-HELICAL_ANGLE_UB = 30
+HELICAL_ANGLE_UB = 50
 
-HELICAL_LAYER_LB = 3
-HELICAL_LAYER_UB = 15
+HELICAL_LAYER_LB = 2
+HELICAL_LAYER_UB = 17
 
 HOOP_LAYER_LB = 1
 HOOP_LAYER_UB = 15
@@ -12,11 +12,11 @@ HOOP_LAYER_UB = 15
 TENSILE_STRENGTH_LB = 600
 TENSILE_STRENGTH_UB = 1100
 
-E1_LB = 95
-E1_UB = 110
+E1_LB = 20
+E1_UB = 120
 
 YOUNGS_MODULUS_LB = 55
-YOUNGS_MODULUS_UB = 150
+YOUNGS_MODULUS_UB = 200
 
 POISSONS_RATIO_LB = 0.25
 POISSONS_RATIO_UB = 0.35
@@ -27,14 +27,14 @@ YEILD_STRENGTH_UB = 250
 ULT_STRENGTH_LB = 310
 ULT_STRENGTH_UB = 370
 
-LINER_THICKNESS_LB = 0.4
-LINER_THICKNESS_UB = 0.6
+LINER_THICKNESS_LB = 0.2
+LINER_THICKNESS_UB = 0.8
 
-DIAMETER_LB = 120
-DIAMETER_UB = 150
+DIAMETER_LB = 100
+DIAMETER_UB = 180
 
-DOLIY_LB = 7
-DOLIY_UB = 25
+DOLIY_LB = 1
+DOLIY_UB = 20
 
 input_cols = ['bust_pressure',  'tensile_str', 'e1_gpa', 'youngs_modulus', 'poision_ratio', 'yeild_strength', 'diameter', 'ult_tensile_strength']
 output_cols = ['helical_angle', 'helical_layer_count', 'hoop_layer_count', 'liner_thickness', 'doily_layers']
